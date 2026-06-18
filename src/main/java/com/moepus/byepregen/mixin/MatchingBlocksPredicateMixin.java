1f8cf9dab88a2408270b5fae3b46f0c93d932f1c
MoePus <547007249@qq.com>
2026-06-18 22:49:57 +0900

    fast block matching

---
+++ src/main/java/com/moepus/byepregen/mixin/MatchingBlocksPredicateMixin.java
@@ -0,0 +1,68 @@
+package com.moepus.byepregen.mixin;
+
+import java.util.List;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = "net.minecraft.world.level.levelgen.blockpredicates.MatchingBlocksPredicate", remap = false)
+public abstract class MatchingBlocksPredicateMixin {
+    @Shadow
+    @Final
+    private HolderSet<Block> blocks;
+
+    @Unique
+    private Block[] bpg$fastBlocks;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void bpg$initFastBlocks(Vec3i offset, HolderSet<Block> blocks, CallbackInfo ci) {
+        List<Holder<Block>> holders = blocks.unwrap().right().orElse(null);
+        if (holders == null) {
+            return;
+        }
+
+        Block[] directBlocks = new Block[holders.size()];
+        for (int i = 0, size = holders.size(); i < size; ++i) {
+            directBlocks[i] = holders.get(i).value();
+        }
+
+        this.bpg$fastBlocks = directBlocks;
+    }
+
+    /**
+     * @author MoePus, Codex
+     * @reason Avoid HolderSet/Holder indirection in hot worldgen block predicate checks.
+     */
+    @Overwrite
+    protected boolean test(BlockState state) {
+        Block[] fastBlocks = this.bpg$fastBlocks;
+        if (fastBlocks != null) {
+            Block block = state.getBlock();
+            int size = fastBlocks.length;
+            if (size == 1) {
+                return block == fastBlocks[0];
+            }
+            if (size == 2) {
+                return block == fastBlocks[0] || block == fastBlocks[1];
+            }
+            for (int i = 0; i < size; ++i) {
+                if (block == fastBlocks[i]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        return state.is(this.blocks);
+    }
+}
