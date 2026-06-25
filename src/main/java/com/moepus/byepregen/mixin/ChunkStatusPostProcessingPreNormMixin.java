9290fa63cdc07f8b44ea141752137f0309f3146b
MoePus <547007249@qq.com>
2026-06-25 22:24:05 +0900

    fix serverside crash with C2MEF

--- src/main/java/com/moepus/byepregen/mixin/ChunkStatusPostProcessingPreNormMixin.java
+++ src/main/java/com/moepus/byepregen/mixin/ChunkStatusPostProcessingPreNormMixin.java
@@ -1,12 +1,9 @@
 package com.moepus.byepregen.mixin;
 
-import com.mojang.datafixers.util.Either;
 import com.moepus.byepregen.PostProcess.PostProcessGenerationOptimizer;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.Function;
-import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ThreadedLevelLightEngine;
 import net.minecraft.world.level.chunk.ChunkAccess;
@@ -22,25 +19,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class ChunkStatusPostProcessingPreNormMixin {
     @Inject(
             method = "generate",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/chunk/ChunkStatus$GenerationTask;doWork(Lnet/minecraft/world/level/chunk/ChunkStatus;Ljava/util/concurrent/Executor;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplateManager;Lnet/minecraft/server/level/ThreadedLevelLightEngine;Ljava/util/function/Function;Ljava/util/List;Lnet/minecraft/world/level/chunk/ChunkAccess;)Ljava/util/concurrent/CompletableFuture;"
-            )
+            at = @At(value = "HEAD")
     )
     private void bpg$preNormalizeFullChunkPostProcessing(
             Executor executor,
             ServerLevel level,
             ChunkGenerator generator,
-            StructureTemplateManager structureTemplateManager,
+            StructureTemplateManager stm,
             ThreadedLevelLightEngine lightEngine,
-            Function<ChunkAccess, CompletableFuture<Either<ChunkAccess, ChunkHolder.ChunkLoadingFailure>>> fullChunkConverter,
+            Function<?, ?> function,
             List<ChunkAccess> chunks,
-            CallbackInfoReturnable<CompletableFuture<Either<ChunkAccess, ChunkHolder.ChunkLoadingFailure>>> cir
+            CallbackInfoReturnable<?> cir
     ) {
         if ((Object) this != ChunkStatus.FULL) {
             return;
         }
-
         ChunkAccess chunk = chunks.get(chunks.size() / 2);
         PostProcessGenerationOptimizer.preNormalizeAndFilterChunkLocalPostProcessingLists(chunk, chunk.getPostProcessing());
     }
