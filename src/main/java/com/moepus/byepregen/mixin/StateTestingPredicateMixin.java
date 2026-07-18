285f524eb70eefb1308339d483b0e292a4a9068d
MoePus <547007249@qq.com>
2026-07-18 20:53:29 +0900

    lift my priority

--- src/main/java/com/moepus/byepregen/mixin/StateTestingPredicateMixin.java
+++ src/main/java/com/moepus/byepregen/mixin/StateTestingPredicateMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(value = StateTestingPredicate.class, remap = false)
+@Mixin(value = StateTestingPredicate.class, priority = 1100, remap = false)
 public abstract class StateTestingPredicateMixin implements FastStateTestingPredicate {
     @Shadow
     @Final
