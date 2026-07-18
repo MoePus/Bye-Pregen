a100b7a56c0ada511cdbab6b46f2eef6fa08ff90
MoePus <547007249@qq.com>
2026-07-18 12:48:02 +0900

    Remove FastBiomePalettedContainer

--- src/main/java/com/moepus/byepregen/mixin/ChunkAccessArenaMixin.java
+++ src/main/java/com/moepus/byepregen/mixin/ChunkAccessArenaMixin.java
@@ -4,7 +4,6 @@ import com.moepus.byepregen.Config;
 import com.moepus.byepregen.ConfigParser;
 import com.moepus.byepregen.PaletteContainer.ArenaPelette.ArenaBlockStatePalettedContainer;
 import com.moepus.byepregen.PaletteContainer.FastPalette.FastBlockStatePalettedContainer;
-import com.moepus.byepregen.PaletteContainer.FastPalette.FastBiomePalettedContainer;
 import javax.annotation.Nullable;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
@@ -106,7 +105,7 @@ public abstract class ChunkAccessArenaMixin {
 
     @Unique
     private PalettedContainer<Holder<Biome>> byepregen$createBiomeContainer(Registry<Biome> biomeRegistry) {
-        return new FastBiomePalettedContainer(
+        return new PalettedContainer<>(
                 biomeRegistry.asHolderIdMap(),
                 biomeRegistry.getHolderOrThrow(Biomes.PLAINS),
                 PalettedContainer.Strategy.SECTION_BIOMES
