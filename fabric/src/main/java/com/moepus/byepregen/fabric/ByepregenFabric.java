4eb9e611a9cbeed1d4c0729b6ddbee99a890940e
MoePus <547007249@qq.com>
2026-08-24 23:38:48 +0900

    Disable YA light when ScalableLux is installed

--- fabric/src/main/java/com/moepus/byepregen/fabric/ByepregenFabric.java
+++ fabric/src/main/java/com/moepus/byepregen/fabric/ByepregenFabric.java
@@ -1,13 +1,23 @@
 package com.moepus.byepregen.fabric;
 
+import com.moepus.byepregen.YALightCompatibility;
 import com.moepus.byepregen.config.ConfigManager;
 import com.moepus.byepregen.yalight.engine.YABlockStateLightClass;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class ByepregenFabric implements ModInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ByepregenFabric.class);
+
     @Override
     public void onInitialize() {
         if (ConfigManager.getConfig().lighting().ya().enabled()) {
+            if (YALightCompatibility.isScalableLuxLoaded()) {
+                LOGGER.warn("ScalableLux is installed, so ByePregen YA light has been disabled "
+                        + "despite lighting.ya.enabled=true");
+                return;
+            }
             YABlockStateLightClass.initialize();
         }
     }
--- neoforge/src/main/java/com/moepus/byepregen/Byepregen.java
+++ neoforge/src/main/java/com/moepus/byepregen/Byepregen.java
@@ -6,16 +6,25 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(Byepregen.MODID)
 public class Byepregen {
     public static final String MODID = "byepregen";
+    private static final Logger LOGGER = LoggerFactory.getLogger(Byepregen.class);
+
     public Byepregen(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(Byepregen::onLoadComplete);
     }
 
     private static void onLoadComplete(FMLLoadCompleteEvent event) {
         if (ConfigManager.getConfig().lighting().ya().enabled()) {
+            if (YALightCompatibility.isScalableLuxLoaded()) {
+                LOGGER.warn("ScalableLux is installed, so ByePregen YA light has been disabled "
+                        + "despite lighting.ya.enabled=true");
+                return;
+            }
             event.enqueueWork(() -> YABlockStateLightClass.initialize());
         }
     }
