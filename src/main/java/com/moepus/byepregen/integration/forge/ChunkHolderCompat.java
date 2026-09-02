8e4f7999833a5bb8e96be19004f4969935c38d05
MoePus <547007249@qq.com>
2026-09-02 23:56:13 +0900

    remove fast get chunk

--- src/main/java/com/moepus/byepregen/integration/forge/ChunkHolderCompat.java
+++ src/main/java/com/moepus/byepregen/integration/forge/ChunkHolderCompat.java
@@ -1,28 +1,21 @@
 package com.moepus.byepregen.integration.forge;
 
 import com.mojang.datafixers.util.Either;
-import com.moepus.byepregen.mixin.accessor.server.chunk.ChunkHolderForgeAccessor;
 import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkStatus;
-import net.minecraft.world.level.chunk.LevelChunk;
 
 public final class ChunkHolderCompat {
     private ChunkHolderCompat() {
     }
 
     public static ChunkAccess getIfPresent(ChunkHolder holder, ChunkStatus status) {
-        LevelChunk loading = ((ChunkHolderForgeAccessor) holder).byepregen$getCurrentlyLoading();
-        if (loading != null) {
-            return loading;
-        }
         Either<ChunkAccess, ChunkHolder.ChunkLoadingFailure> result =
                 holder.getFutureIfPresent(status).getNow(null);
         return result == null ? null : result.left().orElse(null);
     }
 
     public static ChunkAccess getLatest(ChunkHolder holder) {
-        LevelChunk loading = ((ChunkHolderForgeAccessor) holder).byepregen$getCurrentlyLoading();
-        return loading != null ? loading : holder.getLastAvailable();
+        return holder.getLastAvailable();
     }
 }
